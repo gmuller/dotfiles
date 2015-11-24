09bf9f411ceb5408bc733767b38ab2cf4b10fd87
Grant Muller <grantmuller@gmail.com>
2015-11-24 15:44:48 -0500

    added installation instructions to homebrew list

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -13,6 +13,17 @@ then
 fi
 
 # Install homebrew packages
-brew install grc coreutils spark
+brew install grc coreutils spark brew-cask ghc cask cabal-install zsh libsndfile mongodb maven30 libsamplerate liblo jack
+
+brew install Caskroom/cask/anki
+brew install Caskroom/cask/atom
+brew install Caskroom/cask/eclipse-java
+brew install Caskroom/cask/evernote
+brew install Caskroom/cask/github-desktop
+brew install Caskroom/cask/iterm2
+brew install Caskroom/cask/libreoffice
+brew install Caskroom/cask/robomongo
+brew install Caskroom/cask/viscosity
+brew install Caskroom/cask/spotify
 
 exit 0
