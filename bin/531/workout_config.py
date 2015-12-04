adfd26031f9d6f2e00a9e49e4715bdae7cd42b51
gmuller <grantmuller@gmail.com>
2015-12-04 17:28:57 -0500

    updated workout parameters

--- bin/531/workout_config.py
+++ bin/531/workout_config.py
@@ -1,15 +1,11 @@
 [grant]
-dips = 140
-squats = 265
+squats = 275
 deadlift = 405
 bench = 240
 press = 175
-pullups = 90
 
 [cary]
-dips = 30
 squats = 205
 deadlift = 215
 bench = 140
 press = 100
-pullups = 5
