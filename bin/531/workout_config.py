3aab39abd3f3fd4bc08f8236c79a02d6886c979e
gmuller <grantmuller@gmail.com>
2016-02-05 15:26:57 -0500

    workout changes

--- bin/531/workout_config.py
+++ bin/531/workout_config.py
@@ -1,15 +1,11 @@
 [grant]
-dips = 140
-squats = 265
-deadlift = 405
-bench = 240
+squats = 285
+deadlift = 425
+bench = 255
 press = 175
-pullups = 90
 
 [cary]
-dips = 30
 squats = 205
-deadlift = 215
-bench = 140
-press = 100
-pullups = 5
+deadlift = 210
+bench = 130
+press = 95
