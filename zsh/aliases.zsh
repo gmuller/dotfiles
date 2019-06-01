5187bf33f984af0e4cfe100d3792b79e675afe2e
Grant Muller <grantmuller@thenewsendai.lan>
2019-06-01 11:04:05 -0400

    big cleanup to remove oh-my-zsh dependency and other stuff I was no longer using

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,3 @@
-alias zshconfig="vi ~/.zshrc"
-alias ohmyzsh="vi ~/.oh-my-zsh"
 alias battery="pmset -g batt"
 alias reload!='. ~/.zshrc'
 alias captainslog='vi +Goyo ~/Dropbox/logs/captains_log.txt'
--- zsh/functions.zsh
+++
@@ -1,3 +0,0 @@
-jrnlimport () {
-    echo `stat -f %Sm -t '%d %b %Y at %H:%M: ' $1` `cat $1` | jrnl
-}
