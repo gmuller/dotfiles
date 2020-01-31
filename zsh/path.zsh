85ed8d2d7ce23335566d5b23c5b56cfac211bc0e
Grant Muller <grantmuller@gmail.com>
2020-01-31 13:49:55 -0500

    changes to vim and zsh

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,1 +1,1 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.dotfiles/bin:$PATH:$HOME/.ghcup/bin"
+export PATH="/usr/local/opt/python/libexec/bin:./bin:/usr/local/bin:/usr/local/sbin:$HOME/.dotfiles/bin:$PATH:$HOME/.ghcup/bin"
