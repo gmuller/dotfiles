0c19828e07443891ec6d657c196e8abe06055db7
Grant Muller <grantmuller@gmail.com>
2020-04-29 17:26:48 -0400

    mostly changes to environment and an alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,4 @@ alias battery="pmset -g batt"
 alias reload!='. ~/.zshrc'
 alias captainslog='vi +Goyo ~/Dropbox/logs/captains_log.txt'
 alias ia="open -b pro.writer.mac"
+alias serve="python -m http.server"
