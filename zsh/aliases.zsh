110ec78dd4d66a617fdb3078322b8393126e1351
Grant Muller <grantmuller@gmail.com>
2020-05-01 10:00:20 -0400

    more changes for a better vi experience

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,4 @@ alias reload!='. ~/.zshrc'
 alias captainslog='vi +Goyo ~/Dropbox/logs/captains_log.txt'
 alias ia="open -b pro.writer.mac"
 alias serve="python -m http.server"
+alias zettel="vim -c VimwikiIndex -c Goyo"
