2a2cdfeb02c572171ed515912381a1b21abd9033
Grant Muller <grantmuller@gmail.com>
2020-06-02 20:40:35 -0400

    adding search by name to movielinker

--- bin/movielinker.py
+++ bin/movielinker.py
@@ -6,8 +6,9 @@ import os
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument("-f", "--file-name", dest="filename", help="Where to write the output file")
-parser.add_argument("-m", "--movie-id", dest="movie_id", help="Movie ID to Link with")
+parser.add_argument("-f", "--file-name", dest="filename", help="Where to write the output file", default="output.csv")
+parser.add_argument("-i", "--movie-id", dest="movie_id", help="Movie ID to link to")
+parser.add_argument("-m", "--movie-name", dest="movie_name", help="Movie name to link to")
 args = parser.parse_args()
 
 
@@ -15,7 +16,22 @@ filename = args.filename
 tmdb.API_KEY = os.environ['TMDB_KEY']
 seen_list_id = os.environ['TMDB_SEEN_ID']
 a = tmdb.Lists(seen_list_id)
-movie = tmdb.Movies(args.movie_id)
+
+movie_id = args.movie_id
+
+if args.movie_name != None:
+    search = tmdb.Search()
+    response = search.movie(query=args.movie_name);
+    for s in search.results:
+        print(s['id'], s['title'], s['release_date'], s['popularity'])
+    movie_id = input("\nWhich Movie ID did you mean?\n")
+
+movie = tmdb.Movies(movie_id)
+
+for c in movie.credits()['cast']:
+    print(c['id'], c['name'])
+
+excluded_collaborator = input("\nExclude a Collaborator?\n")
 
 response = movie.info()
 list_response = a.info()
