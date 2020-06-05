fa52fbfc3b7f309e9bf1ec3711d70257480635cf
Grant Muller <grantmuller@gmail.com>
2020-06-05 15:38:28 -0400

    more changes to movielinker to make it a little easier to use

--- bin/movielinker.py
+++ bin/movielinker.py
@@ -3,18 +3,23 @@
 import tmdbsimple as tmdb
 import csv
 import os
+import configparser
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument("-f", "--file-name", dest="filename", help="Where to write the output file", default="output.csv")
 parser.add_argument("-i", "--movie-id", dest="movie_id", help="Movie ID to link to")
 parser.add_argument("-m", "--movie-name", dest="movie_name", help="Movie name to link to")
 args = parser.parse_args()
 
+home = os.path.expanduser("~")
+config = configparser.ConfigParser()
+config.read(os.path.join(home, '.movielinker'))
+username = config['movielinker']['username']
+password = config['movielinker']['password']
+api_key = config['movielinker']['api_key']
+seen_list_id = config['movielinker']['seen_list_id']
 
-filename = args.filename
-tmdb.API_KEY = os.environ['TMDB_KEY']
-seen_list_id = os.environ['TMDB_SEEN_ID']
+tmdb.API_KEY = api_key
 a = tmdb.Lists(seen_list_id)
 
 movie_id = args.movie_id
@@ -22,9 +27,15 @@ movie_id = args.movie_id
 if args.movie_name != None:
     search = tmdb.Search()
     response = search.movie(query=args.movie_name);
+    index = 0
+    foundMovies = []
     for s in search.results:
-        print(s['id'], s['title'], s['release_date'], s['popularity'])
-    movie_id = input("\nWhich Movie ID did you mean?\n")
+        foundMovies.append({'id': s['id'], 'title': s['title']})
+        print(index, s['title'], s['release_date'])
+        index+=1
+    movie_index = int(input("\nWhich Movie ID did you mean?\n"))
+    movie_id = foundMovies[movie_index]['id']
+    print("\nGetting cast list for", foundMovies[movie_index]['title'],"\n")
 
 movie = tmdb.Movies(movie_id)
 
@@ -35,7 +46,7 @@ for c in movie.credits()['cast']:
     print(index, c['name'])
     index+=1
 
-excluded_collaborator_index = int(input("\nExclude a Collaborator?\n"))
+excluded_collaborator_index = int(input("\nExclude a cast member?\n"))
 excluded_collaborator = collaboratorList[excluded_collaborator_index]
 print("Excluding", excluded_collaborator['name'])
 
@@ -58,6 +69,7 @@ for c in movie.credits()['cast']:
             p['collaborators'] = {c['name']}
             cache[p['id']] = p;
 
+filename = "".join(x for x in movie.title if x.isalnum()) + ".csv"
 with open(filename, 'w') as csv_file:
     fields = ['id', 'title', 'collaborators', 'vote_average', 'vote_count', 'release_date', 'overview']
     writer = csv.DictWriter(csv_file, fieldnames=fields, extrasaction='ignore')
